b6dd876f8853389295163ecaf1f751295f2bbfa9
Vishal Kumar Yadav <vishalkumarpb10@gmail.com>
2025-11-26 17:20:37 +0530

    Added commit

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ const app = express();
 app.get('/', (req,res) => {
 
     res.send ("<h2> Hello from node ,From the inside docker Container.....</h2>")
+    console.log("App is running on staging environment...");
 } );
 
 app.listen(
